3ccdb1b1cc64cc7849750d5a637a6476ab3952f9
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-02-17 14:28:14 +0600

    loaded category wise products

--- js/script.js
+++ js/script.js
@@ -5,43 +5,6 @@ const loadTrending = () => {
     .then((json) => displayTrending(json));
 };
 
-const loadAllProducts = () => {
-  fetch("https://fakestoreapi.com/products")
-    .then((res) => res.json())
-    .then((json) => displayAllProducts(json));
-}
-
-const displayAllProducts = (products) => {
-  const allProducts = document.getElementById("all-products");
-  allProducts.innerHTML = "";
-
-  products.forEach(product => {
-    productCard = document.createElement("div");
-    productCard.innerHTML = `
-    <div class="card bg-base-100 max-w-96 shadow-sm">
-        <div class="h-72  bg-gray-400">
-            <img class="h-full mx-auto items-center" src="${product.image}" alt="${product.category}" />
-        </div>
-        <div class="p-5 space-y-4">
-            <div class="flex justify-between">
-                <p class="text-[#4F39F6] bg-[#4F39F620] px-2 rounded-xl">${product.category}</p>
-                <p><span class="text-yellow-500 mr-1"><i class="fa-solid fa-star"></i></span>${product.rating.rate}
-                    (${product.rating.count})</p>
-            </div>
-            <h2 class="card-title">${product.title}</h2>
-            <p class="font-bold">$${product.price}</p>
-            <div class="flex justify-between">
-                <button class="btn"><i class="fa-solid fa-circle-info"></i>Details</button>
-                <button class="btn btn-primary"><i class="fa-solid fa-cart-shopping"></i>Buy Now</button>
-            </div>
-        </div>
-    </div>
-    `
-
-    allProducts.append(productCard)
-  })
-}
-
 const displayTrending = (trendingData) => {
   const trending = document.getElementById("trending");
   trending.innerHTML = "";
@@ -76,6 +39,16 @@ const displayTrending = (trendingData) => {
 // Home page ends
 
 // Products page starts
+ const  showProducts = () => {
+  document.getElementById("products-section").classList.remove("hidden")
+  document.getElementById("main-section").classList.add("hidden")
+ }
+ const  showMain = () => {
+  document.getElementById("products-section").classList.add("hidden")
+  document.getElementById("main-section").classList.remove("hidden")
+ }
+
+
 const loadCategory = () => {
   const url = "https://fakestoreapi.com/products/categories";
   fetch(url)
@@ -83,20 +56,66 @@ const loadCategory = () => {
     .then((data) => displayCategory(data));
 };
 
+const loadAllProducts = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((json) => displayAllProducts(json));
+};
+
+const loadProductsByCategory = (category) => {
+  const url = `https://fakestoreapi.com/products/category/${category}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayAllProducts(data));
+};
+
+const displayAllProducts = (products) => {
+  const allProducts = document.getElementById("all-products");
+  allProducts.innerHTML = "";
+
+  products.forEach((product) => {
+    productCard = document.createElement("div");
+    productCard.innerHTML = `
+    <div class="card bg-base-100 max-w-96 shadow-sm">
+        <div class="h-72  bg-gray-400">
+            <img class="h-full mx-auto items-center" src="${product.image}" alt="${product.category}" />
+        </div>
+        <div class="p-5 space-y-4">
+            <div class="flex justify-between">
+                <p class="text-[#4F39F6] bg-[#4F39F620] px-2 rounded-xl">${product.category}</p>
+                <p><span class="text-yellow-500 mr-1"><i class="fa-solid fa-star"></i></span>${product.rating.rate}
+                    (${product.rating.count})</p>
+            </div>
+            <h2 class="card-title">${product.title}</h2>
+            <p class="font-bold">$${product.price}</p>
+            <div class="flex justify-between">
+                <button class="btn"><i class="fa-solid fa-circle-info"></i>Details</button>
+                <button class="btn btn-primary"><i class="fa-solid fa-cart-shopping"></i>Buy Now</button>
+            </div>
+        </div>
+    </div>
+    `;
+
+    allProducts.append(productCard);
+  });
+};
+
 const displayCategory = (categories) => {
   const productCategory = document.getElementById("product-category");
   productCategory.innerHTML = `<button onclick="loadAllProducts()" class="btn btn-outline">All</button>`;
 
   categories.forEach((category) => {
+    const str = encodeURIComponent(category);
+
     const categoryBtn = document.createElement("div");
-    categoryBtn.innerHTML = `<button class="btn btn-outline capitalize">${category}</button>`;
+    categoryBtn.innerHTML = `<button onclick="loadProductsByCategory(&quot;${str}&quot;)" class="btn btn-outline capitalize">${category}</button>`;
 
     productCategory.appendChild(categoryBtn);
   });
 };
 
-
 // Products page ends
 
 loadTrending();
 loadCategory();
+loadAllProducts();
